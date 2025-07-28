5273cc0588c48a25c91f03399961e512abd46936
Mr-Hijaks <99844897+Mr-Hijaks@users.noreply.github.com>
2025-07-28 13:03:02 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: { sans: ['Inter', 'system-ui', 'sans-serif'] },
+      animation: { 'spin': 'spin 1s linear infinite' }
+    }
+  },
+  plugins: []
+}
